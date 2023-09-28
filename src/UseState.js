7441a95c1fc86957fe450b1fae731455b5e8f367
theninexl <thenine@thenine.es>
2023-09-28 16:51:00 +0200

    imperative states with useState

--- src/UseState.js
+++ src/UseState.js
@@ -11,6 +11,8 @@ function UseState({ name }){
     value: '',
     error: false,
     loading: false,
+    deleted: false,
+    confirmed: false,
   });
 
 
@@ -26,6 +28,7 @@ function UseState({ name }){
             ...state,
             error: false,
             loading: false,
+            confirmed: true,
           })
         } else {
           setState({
@@ -42,38 +45,78 @@ function UseState({ name }){
     console.log('terminando el efecto');
   },[state.loading])
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor, escribe el código de seguridad.</p>
-      {(state.error && !state.loading) && (
-        <p>Error: el código es incorrecto</p>
-      )}
-      {state.loading && (
-        <p>Cargando...</p>
-      )}
-      <input 
-        placeholder='Código de seguridad'
-        value={state.value}
-        onChange={(event) => {
-          // setValue(event.target.value);
-          setState({
-            ...state,
-            value: event.target.value,
-          })
-        }}
-      />
-      <button
-        onClick={() => {
-          // setLoading(true);
-          setState({
-            ...state,
-            loading:true,
-          })
-        }}
-      >Comprobar</button>
-    </div>
-  );
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Por favor, escribe el código de seguridad.</p>
+        {(state.error && !state.loading) && (
+          <p>Error: el código es incorrecto</p>
+        )}
+        {state.loading && (
+          <p>Cargando...</p>
+        )}
+        <input 
+          placeholder='Código de seguridad'
+          value={state.value}
+          onChange={(event) => {
+            // setValue(event.target.value);
+            setState({
+              ...state,
+              value: event.target.value,
+            })
+          }}
+        />
+        <button
+          onClick={() => {
+            // setLoading(true);
+            setState({
+              ...state,
+              loading:true,
+            })
+          }}
+        >Comprobar</button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <>
+        <h2>Eliminar {name}</h2>
+        <p>¿Seguro que quieres eliminar {name}?</p>
+        <button
+          onClick={()=>{
+            setState({
+              ...state,
+              deleted:true,
+            })
+          }}>Si, eliminar</button>
+        <button
+          onClick={()=>{
+            setState({
+              ...state,
+              value:'',
+              confirmed:false
+            })
+          }}>No, volver</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Eliminar {name}</h2>
+        <p>Confirmada la eliminación.</p>
+        <button
+          onClick={()=>{
+            setState({
+              ...state,
+              value:'',
+              confirmed:false,
+              deleted:false,
+            })
+          }}>Restituir al principio</button>
+      </>
+    );
+  }
 }
 
 export { UseState };
